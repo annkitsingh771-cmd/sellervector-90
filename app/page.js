0cae0e4449e0d9eba27ef1efbb5c155593c69eb8
Ankit Singh <annkitsingh771@gmail.com>
2026-04-17 23:15:22 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,15 +1,3 @@
-import Hero from "./Hero";
-import Features from "./Features";
-import About from "./About";
-import Contact from "./Contact";
-
 export default function Home() {
-  return (
-    <>
-      <Hero />
-      <Features />
-      <About />
-      <Contact />
-    </>
-  );
+  return <h1>Working</h1>;
 }
